3abcb508d32f754a1e46299e1270ecf90a967d4e
aman <amanrajasthan123@gmail.com>
2023-11-13 23:43:48 +0530

    Responsive

--- front-end/src/component/home/Home.jsx
+++ front-end/src/component/home/Home.jsx
@@ -1,17 +1,10 @@
 import React from "react";
+import Footer from "../Footer/Footer";
 const Home = () => {
   return (
-    <div>
-      <h1>Aman</h1>
-      <iframe
-        src="/public/dip (1).pdf"
-        target="_blank"
-        frameborder="0"
-      ></iframe>
-      <a href="/public/dip (1).pdf" target="_blank" download>
-        Open our infographic
-      </a>
-    </div>
+    <>
+      <Footer />
+    </>
   );
 };
 
