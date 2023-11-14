2fe03bc7b1072b6778b4644776addd149b0e6db9
vipul-varshney12 <vipulvarahney@gmail.com>
2023-11-14 23:32:50 +0530

    this  about of  Academease

--- front-end/src/component/about/AboutUs.jsx
+++ front-end/src/component/about/AboutUs.jsx
@@ -4,22 +4,56 @@ import './AboutUs.css';
 const AboutUs = () => {
   return (
     <>
-      <div className='about'>
-      <div className='teacher'>
-        <img src='https://www.oxfordlearning.com/wp-content/uploads/2020/04/GettyImages-1035393946-scaled.jpg'></img>
+      
+      <div className="about-container">
+      <div className="about-image">
+        <img src="https://www.bing.com/th?id=OIP.jEeunVRW28s2uBZlXjKQHAHaEt&w=143&h=100&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2" alt="Academmease Image" />
       </div>
-      <div className="nine">
-  <h1>Welcome to Academease</h1>
- <div className='para'>
- <p>Our Vision We envision a world where every student, irrespective of background or circumstance. Through Academease, we aim to empower students to achieve their academic goals with confidence and ease.</p>
- </div>
- <div className='know'>
- <button className="know-more-button">Know More</button>
- </div>
-</div>
+      <div className="about-content">
+        <h1>Welcome to Academmease</h1>
+        
+        <p>
+          Academmease is your ultimate destination for accessing
+          comprehensive online notes to support your learning jo 
+          urney. With a diverse collection of study materials, 
+          we aim to empower students in their pursuit of knowledge.
+          urney. With a diverse collection of study materials, 
+          we aim to empower students in their pursuit of knowledge.
+          urney. With a diverse collection of study materials, 
+          we aim to empower students in their pursuit of knowledge.
+        </p>
+        <button className="know-more-button">Know More</button>
       </div>
+      
+    </div>
+    
+    
     </>
   )
-  // ... rest of the component code
+  
 };
 export default AboutUs;
+/*
+ <div className="about-container">
+      <div className="about-image">
+        <img src="https://www.bing.com/th?id=OIP.jEeunVRW28s2uBZlXjKQHAHaEt&w=143&h=100&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2" alt="Academmease Image" />
+      </div>
+      <div className="about-content">
+        <h1>Welcome to Academmease</h1>
+        
+        <p>
+          Academmease is your ultimate destination for accessing
+          comprehensive online notes to support your learning jo 
+          urney. With a diverse collection of study materials, 
+          we aim to empower students in their pursuit of knowledge.
+          urney. With a diverse collection of study materials, 
+          we aim to empower students in their pursuit of knowledge.
+          urney. With a diverse collection of study materials, 
+          we aim to empower students in their pursuit of knowledge.
+        </p>
+        <button className="know-more-button">Know More</button>
+      </div>
+      
+    </div>
+
+*/
