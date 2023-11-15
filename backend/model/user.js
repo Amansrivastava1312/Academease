28cf0d87af5c0857447b7c54f8abe6d161c13ed4
aman <amanrajasthan123@gmail.com>
2023-11-16 00:21:50 +0530

    Check Password

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,43 +1,44 @@
 const mongoose = require("mongoose");
 const { randomBytes, createHash } = require("crypto");
-
 const UserSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "Please Enter the Full Name"]
-    },
-    email: {
-        type: String,
-        required: [true, "Please Enter the Email"],
-        unique:true,
-    },
-    roll: {
-        type: Number,
-        required: [true, "Please Enter the University Roll Number"],
-        unique:true,
-    },
-    salt: {
-        type: String,
-    },
+  name: {
+    type: String,
+    required: [true, "Please Enter the Full Name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please Enter the Email"],
+    unique: true,
+  },
+  roll: {
+    type: Number,
+    required: [true, "Please Enter the University Roll Number"],
+    unique: true,
+  },
+  salt: {
+    type: String,
+  },
 
-    password: {
-        type: String,
-        required: [true, "Please Enter the Password"],
-    },
-})
+  password: {
+    type: String,
+    required: [true, "Please Enter the Password"],
+  },
+});
 
-UserSchema.pre("save", function(next) {
-    const user = this;
-    if (!user.isModified("password")) return;
+// UserSchema.pre("save", function (next) {
+//   const user = this;
+//   if (!user.isModified("password")) return;
 
-    const salt = randomBytes(16).toString();
-    const hashedPassword = createHash("sha256", salt).update(user.password).digest("hex");
+//   const salt = randomBytes(16).toString();
+//   const hashedPassword = createHash("sha256", salt)
+//     .update(user.password)
+//     .digest("hex");
 
-    this.password = hashedPassword;
-    this.salt = salt;
+//   this.password = hashedPassword;
+//   this.salt = salt;
 
-    next();
-})
+//   next();
+// });
 
 const User = mongoose.model("user", UserSchema);
 
