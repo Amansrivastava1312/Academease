d4fbd08eca8e54b68c29e290119939c81d25c2d6
yash <yash@yash>
2023-11-16 22:53:40 +0530

    team

--- front-end/src/component/Team/Team.jsx
+++ front-end/src/component/Team/Team.jsx
@@ -1,100 +1,139 @@
-import React from 'react'
-import { FaRocket } from "react-icons/fa";
+import React from "react";
+import { FaRocket,FaInstagram,FaGithub } from "react-icons/fa";
+import { IoIosCheckmarkCircle } from "react-icons/io";
+import { FaFacebookF } from "react-icons/fa6";
+import { SiLeetcode } from "react-icons/si";
+import { CiHeart } from "react-icons/ci";
 
-import "./Team.css"
+import "./Team.css";
 
 const Team = () => {
   return (
-      <>
-     <section id="team">
-  <h1 className="heading">
-    <FaRocket />
- <strong>Our Team</strong>
-  </h1>
-  <div className="container-t">
-    <div className="box-t">
-      <div className="top-bar-t" />
-      <div className="nav-t">
-        <i className="verify fas fa-check-circle" />
-        <input type="checkbox" className="heart-btn" id="heart-btn" />
-        <label className="heart" />
-      </div>
-      <div className="details">
-        <img src="images/team1.jpg" alt />
-        <strong>Emma Olivia</strong>
-        <p>Emmaolivia@gmail.com</p>
-      </div>
-      <div className="btn">
-        <a href="#"><i className="fab fa-facebook-f" /></a>
-        <a href="#"><i className="fab fa-twitter" /></a>
-        <a href="#"><i className="fab fa-instagram" /></a>
-        <a href="#"><i className="fab fa-youtube" /></a>
-      </div>
-    </div>
-    <div className="box-t">
-      <div className="top-bar-t" />
-      <div className="nav-t">
-        <i className="verify fas fa-check-circle" />
-        <input type="checkbox" className="heart-btn" id="heart-btn" />
-        <label className="heart" />
-      </div>
-      <div className="details">
-        <img src="images/team2.jpg" alt />
-        <strong>Sofia Adward</strong>
-        <p>Sofiaadward@gmail.com</p>
-      </div>
-      <div className="btn">
-        <a href="#"><i className="fab fa-facebook-f" /></a>
-        <a href="#"><i className="fab fa-twitter" /></a>
-        <a href="#"><i className="fab fa-instagram" /></a>
-        <a href="#"><i className="fab fa-youtube" /></a>
-      </div>
-    </div>
-    <div className="box-t">
-      <div className="top-bar-t" />
-      <div className="nav-t">
-        <i className="verify fas fa-check-circle" />
-        <input type="checkbox" className="heart-btn" id="heart-btn" />
-        <label className="heart" />
-      </div>
-      <div className="details">
-        <img src="images/team3.jpg" alt />
-        <strong>Touseeq Ijaz</strong>
-        <p>Touseeqijaz@gmail.com</p>
-      </div>
-      <div className="btn">
-        <a href="#"><i className="fab fa-facebook-f" /></a>
-        <a href="#"><i className="fab fa-twitter" /></a>
-        <a href="#"><i className="fab fa-instagram" /></a>
-        <a href="#"><i className="fab fa-youtube" /></a>
-      </div>
-    </div>
-    <div className="box-t">
-      <div className="top-bar-t" />
-      <div className="nav-t">
-        <i className="verify fas fa-check-circle" />
-        <input type="checkbox" className="heart-btn" id="heart-btn" />
-        <label className="heart" />
-      </div>
-      <div className="details">
-        <img src="images/team4.jpg" alt />
-        <strong>Amelia Alex</strong>
-        <p>Ameliaalex@gmail.com</p>
-      </div>
-      <div className="btn">
-        <a href="#"><i className="fab fa-facebook-f" /></a>
-        <a href="#"><i className="fab fa-twitter" /></a>
-        <a href="#"><i className="fab fa-instagram" /></a>
-        <a href="#"><i className="fab fa-youtube" /></a>
-      </div>
-    </div>
-  </div>
-</section>
+    <div className="team-container">
+      <section id="team">
+        <h1 className="heading">
+          <FaRocket />
+          <strong>Our Team</strong>
+        </h1>
+        <div className="container-t">
+          <div className="box-t">
+            <div className="top-bar-t" />
+            <div className="nav-t">
+              <h6>
+                <IoIosCheckmarkCircle />
+              </h6>
+              <input type="checkbox" className="heart-btn" id="heart-btn" />
+              <CiHeart />
+              
+            </div>
+            <div className="details">
+              <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D" alt />
+              <strong>Emma Olivia</strong>
+              <p>Emmaolivia@gmail.com</p>
+            </div>
+            <div className="btn">
+              <a href="#">
+                <FaFacebookF />
+              </a>
+              <a href="#">
+                <FaGithub />
+              </a>
+              <a href="#">
+                <FaInstagram />
+              </a>
+              <a href="#">
+                <SiLeetcode />
+              </a>
+            </div>
+          </div>
+          <div className="box-t">
+            <div className="top-bar-t" />
+            <div className="nav-t">
+              <IoIosCheckmarkCircle />
 
+              <input type="checkbox" className="heart-btn" id="heart-btn" />
+              <CiHeart />
+            </div>
+            <div className="details">
+              <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D" alt />
+              <strong>Sofia Adward</strong>
+              <p>Sofiaadward@gmail.com</p>
+            </div>
+            <div className="btn">
+              <a href="#">
+                <FaFacebookF />
+              </a>
+              <a href="#">
+                <FaGithub />
+              </a>
+              <a href="#">
+                <FaInstagram />
+              </a>
+              <a href="#">
+                <SiLeetcode />
+              </a>
+            </div>
+          </div>
+          <div className="box-t">
+            <div className="top-bar-t" />
+            <div className="nav-t">
+              <IoIosCheckmarkCircle />
 
-      
-      </>
-  )
-}
+              <input type="checkbox" className="heart-btn" id="heart-btn" />
+              <CiHeart />
+            </div>
+            <div className="details">
+              <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D" alt />
+              <strong>Touseeq Ijaz</strong>
+              <p>Touseeqijaz@gmail.com</p>
+            </div>
+            <div className="btn">
+              <a href="#">
+                <FaFacebookF />
+              </a>
+              <a href="#">
+                <FaGithub />
+              </a>
+              <a href="#">
+                <FaInstagram />
+              </a>
+              <a href="#">
+                <SiLeetcode />
+              </a>
+            </div>
+          </div>
+          <div className="box-t">
+            <div className="top-bar-t" />
+            <div className="nav-t">
+              <IoIosCheckmarkCircle />
 
-export default Team
+              <input type="checkbox" className="heart-btn" id="heart-btn" />
+              <CiHeart />
+            </div>
+            <div className="details">
+              <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D" alt />
+              <strong>Amelia Alex</strong>
+              <p>Ameliaalex@gmail.com</p>
+            </div>
+            <div className="btn">
+              <a href="#">
+                <FaFacebookF />
+              </a>
+              <a href="#">
+                <FaGithub />
+              </a>
+              <a href="#">
+                <FaInstagram />
+              </a>
+              <a href="#">
+                <SiLeetcode />
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Team;
