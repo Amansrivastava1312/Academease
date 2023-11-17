f78cab9cf9093244f15ca180291f335e0b286254
aman <amanrajasthan123@gmail.com>
2023-11-17 13:35:14 +0530

    Home

--- front-end/src/component/Layout/Layout.jsx
+++ front-end/src/component/Layout/Layout.jsx
@@ -4,7 +4,8 @@ import Header from "./Header";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { NavLink, Link } from "react-router-dom";
-
+import { FaUpload } from "react-icons/fa";
+import { IoMdCall } from "react-icons/io";
 const Layout = ({ children }) => {
   return (
     <>
@@ -20,34 +21,84 @@ const Layout = ({ children }) => {
             zIndex: "0",
           }}
         />
-        <h1
+        <div
           style={{
             position: "relative",
-            paddingLeft: "30vh",
-            paddingTop: "40vh",
+            paddingLeft: "10vh",
+            paddingTop: "25vh",
+            display: "flex",
+            flexDirection: "column",
+            // lineHeight: "100%",
+            gap: "5px",
           }}
         >
-          Help,Your Friends <br /> by providing notes.{" "}
-        </h1>
-        <Link to="/upload" style={{ textDecoration: 'none' }}>
-      <button
-        style={{
-          position: "relative",
-          marginLeft: "30vh",
-          marginTop: "5px",
-          padding: "20px",
-          borderRadius: "20px",
-          backgroundColor: "transparent",
-          color: "#000",  // Text color
-          border: "2px solid #007BFF",  // Border color
-          transition: "background-color 0.3s",  // Transition for smooth effect
-        }}
-        onMouseOver={(e) => e.target.style.backgroundColor = "#20c997"}
-        onMouseOut={(e) => e.target.style.backgroundColor = "transparent"}
-      >
-        Upload Here
-      </button>
-    </Link>
+          <h5 style={{ color: "white", fontWeight: "bolder" }}>
+            {" "}
+            WELCOME TO ACADEMEASE
+          </h5>
+          <h1
+            style={{
+              color: "whitesmoke",
+              fontFamily: "sans-serif",
+              fontWeight: "bold",
+            }}
+          >
+            Empowering Students with <br /> Shared Knowledge.
+          </h1>
+          <p>
+            Study smarter, share notes: A comprehensive platform for sharing and{" "}
+            <br />
+            accessing study materials.
+          </p>
+
+          <div className="button231" style={{ display: "flex", gap: "20px" }}>
+            <NavLink to={"/upload"}>
+              <button
+                style={{
+                  padding: "7px 5vh",
+
+                  border: "none",
+                  background: "#0ec7bb",
+                  color: "white",
+                }}
+              >
+                Upload Notes <FaUpload style={{ marginLeft: "10px" }} />
+              </button>
+            </NavLink>
+            <NavLink to={"/contact"}>
+              <button
+                style={{
+                  padding: "7px 5vh",
+
+                  border: "none",
+                  // background: "#0ec7bb",
+                  color: "black",
+                }}
+              >
+                Contact Us <IoMdCall style={{ marginLeft: "10px" }} />
+              </button>
+            </NavLink>
+          </div>
+        </div>
+        {/* <Link to="/upload" style={{ textDecoration: "none" }}>
+          <button
+            style={{
+              position: "relative",
+              marginLeft: "30vh",
+              marginTop: "5px",
+              padding: "20px",
+              borderRadius: "20px",
+              backgroundColor: "transparent",
+              color: "#000", // Text color
+              border: "2px solid #007BFF", // Border color
+              transition: "background-color 0.3s", // Transition for smooth effect
+            }}
+            onMouseOver={(e) => (e.target.style.backgroundColor = "#20c997")}
+            onMouseOut={(e) => (e.target.style.backgroundColor = "transparent")}
+          >
+            Upload Here
+          </button>
+        </Link> */}
       </div>
 
       <main style={{ minHeight: "80vh" }}>
