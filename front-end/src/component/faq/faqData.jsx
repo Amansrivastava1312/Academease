534e7c5a2e329fd572bfc4006ec9ebb1e510983e
Vidya Sagar Singh <vidyasagarsingh6171@gmail.com>
2023-11-18 15:41:05 +0530

    Updated the FAQ Data

--- front-end/src/component/faq/faqData.jsx
+++ front-end/src/component/faq/faqData.jsx
@@ -1,26 +1,26 @@
 export const faq = [
   {
-    title: "How to Enroll This Online Courses?",
-    desc: "This is the first item's accordion body. It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
+    title: "What is Academease about?",
+    desc: "Academease is a platform where you can find and share college notes, study materials, and resources to support your academic journey.",
   },
   {
-    title: "Where It is hidden by default, until the collapse?",
-    desc: "It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
+    title: "How can I use Academease?",
+    desc: "Simply browse through the available categories or use the search bar to find specific notes. You can download and contribute notes to help others in their studies.",
   },
   {
-    title: "How It is hidden by default, until the collapse?",
-    desc: "It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
+    title: "Is Academease free to use?",
+    desc: "Yes, Academease is completely free to use. We believe in making educational resources accessible to everyone.",
   },
   {
-    title: "How to Enroll This Online Courses?",
-    desc: "This is the first item's accordion body. It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
+    title: "How can I upload my notes to Academease?",
+    desc: "To upload your notes, create an account, and then go to the --Upload-- section. Follow the instructions to submit your notes, ensuring they meet our content guidelines.",
   },
   {
-    title: "Where It is hidden by default, until the collapse?",
-    desc: "It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
+    title: "I'm having trouble downloading notes. What should I do?",
+    desc: "Ensure that you have a stable internet connection. If the issue persists, contact our support team through the --Contact Us-- page for assistance.",
   },
   {
-    title: "How It is hidden by default, until the collapse?",
-    desc: "It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
+    title: " How can I contribute to the Academease community?",
+    desc: "You can contribute by uploading your notes, providing feedback, and participating in discussions on the forum. Your active involvement helps build a thriving community.",
   },
 ];
