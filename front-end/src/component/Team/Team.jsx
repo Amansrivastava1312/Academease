bfd159e8c31bda359561e9710e56a27b9cd81f97
yash <yash@yash>
2023-11-26 21:52:47 +0530

    temp-details

--- front-end/src/component/Team/Team.jsx
+++ front-end/src/component/Team/Team.jsx
@@ -65,29 +65,29 @@ const Team = () => {
             </div>
             <div className="details">
               <img
-                src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D"
+                src="/images/yash.jpg"
                 alt
               />
               <strong>Yash Singhal</strong>
               <p>Full Stack</p>
             </div>
             <div className="btn">
-              <a href="#">
+              <a href="https://www.facebook.com/yash.singhal.735944">
                 <i>
                   <FaFacebookF />
                 </i>
               </a>
-              <a href="#">
+              <a href="https://github.com/yash5734">
                 <i>
                   <FaGithub />
                 </i>
               </a>
-              <a href="#">
+              <a href="https://www.instagram.com/_this_is_yash/">
                 <i>
                   <FaInstagram />
                 </i>
               </a>
-              <a href="#">
+              <a href="https://leetcode.com/yash5734/">
                 <i>
                   <SiLeetcode />
                 </i>
